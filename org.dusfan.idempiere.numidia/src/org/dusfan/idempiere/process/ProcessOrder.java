3b0868b652b478fa844dcf98d986c0fdab81bc90
dusfan <dusfan@localhost>
2018-05-26 12:42:00 +0100

    Amelioration du processus de Complete/reactivate Order

--- org.dusfan.idempiere.numidia/src/org/dusfan/idempiere/process/ProcessOrder.java
+++ org.dusfan.idempiere.numidia/src/org/dusfan/idempiere/process/ProcessOrder.java
@@ -1,11 +1,10 @@
 /** * Licensed under the KARMA v.1 Law of Sharing. As others have shared freely to you, so shall you share freely back to us. * If you shall try to cheat and find a loophole in this license, then KARMA will exact your share, * and your worldly gain shall come to naught and those who share shall gain eventually above you. * In compliance with previous GPLv2.0 works of Jorg Janke, Low Heng Sin, Carlos Ruiz and contributors. * This Module Creator is an idea put together and coded by Redhuan D. Oon (red1@red1.org) */package org.dusfan.idempiere.process;
 import java.util.List;import org.compiere.model.MOrder;
-import org.compiere.model.Query;import org.compiere.process.ProcessInfoParameter;
+import org.compiere.model.Query;import org.compiere.process.DocAction;import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.Env;
 
-public class ProcessOrder extends SvrProcess {		private int cpt_completed;	/** Invalid = IN */	public static final String STATUS_Invalid = "IN";	/** Completed = CO */	public static final String STATUS_Completed = "CO";	/** Complete = CO */	public static final String ACTION_Complete = "CO";	/** Re-activate = RE */	public static final String ACTION_Re_Activate = "RE";	/** Void = VO */	public static final String ACTION_Void = "VO";
-	private String p_DocAction;
+public class ProcessOrder extends SvrProcess {		private int cpt_completed;	private String p_DocAction;
 	protected void prepare() {
 		ProcessInfoParameter[] para = getParameter();
 		for (ProcessInfoParameter p:para) {
@@ -23,11 +22,11 @@ public class ProcessOrder extends SvrProcess {		private int cpt_completed;	/
 				.setParameters(getAD_PInstance_ID()).list();
 
 		for (MOrder order:lines){
-			String a = order.getDocumentNo();						if (p_DocAction.equals(ACTION_Complete)){				completeOrder(order);			}			if (p_DocAction.equals(ACTION_Re_Activate)){				reActivateOrder(order);			}
-			if (p_DocAction.equals(ACTION_Void)){				voidOrder(order);			}
+			String a = order.getDocumentNo();						if (p_DocAction.equals(DocAction.ACTION_Complete)){				completeOrder(order);			}			if (p_DocAction.equals(DocAction.ACTION_ReActivate)){				reActivateOrder(order);			}
+			if (p_DocAction.equals(DocAction.ACTION_Void)){				voidOrder(order);			}
 			log.info("Selected order ID = "+a);
 
 		}		return "Order processed : "+ cpt_completed;
 
-	}	private void completeOrder (MOrder order){		if (order.getDocStatus().equals(STATUS_Completed))			return;		else {			if (order.processIt(ACTION_Complete))				if ( order.save()){					cpt_completed ++;				}		}			}		private void voidOrder(MOrder order) {		if (!order.getDocStatus().equals(STATUS_Completed))			return;		else {			if (order.processIt(ACTION_Void))				if ( order.save()){					cpt_completed ++;				}		}	}	private void reActivateOrder(MOrder order) {		if (!order.getDocStatus().equals(STATUS_Completed))			return;		else {			if (order.processIt(ACTION_Re_Activate))				if ( order.save()){					cpt_completed ++;				}		}	}
+	}	private void completeOrder (MOrder order){		if (order.getDocStatus().equals(DocAction.ACTION_Complete))			return;		else {			if (order.processIt(DocAction.ACTION_Complete))				if ( order.save()){					cpt_completed ++;				}		}			}		private void voidOrder(MOrder order) {		if (!order.getDocStatus().equals(DocAction.ACTION_Complete))			return;		else {			if (order.processIt(DocAction.ACTION_Void))				if ( order.save()){					cpt_completed ++;				}		}	}	private void reActivateOrder(MOrder order) {		if (!order.getDocStatus().equals(DocAction.ACTION_Complete))			return;		else {			if (order.processIt(DocAction.ACTION_ReActivate))				if ( order.save()){					cpt_completed ++;				}		}	}
 }
