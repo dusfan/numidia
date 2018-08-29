826ee0508ccb43756f6c3cf202247b1764fab9fd
dusfan <dusfan@localhost>
2018-08-29 15:00:35 +0100

    Comptabilisation  des affectation automatique en relation avec l'activit? (posted/not posted)

--- org.dusfan.idempiere.numidia/src/org/dusfan/idempiere/event/EventFact.java
+++ org.dusfan.idempiere.numidia/src/org/dusfan/idempiere/event/EventFact.java
@@ -3,6 +3,8 @@ package org.dusfan.idempiere.event;
 import java.util.Properties;
 
 import org.compiere.acct.FactLine;
+import org.compiere.model.MAllocationHdr;
+import org.compiere.model.MAllocationLine;
 import org.compiere.model.MBankStatement;
 import org.compiere.model.MBankStatementLine;
 import org.compiere.model.MPayment;
@@ -24,6 +26,16 @@ public class EventFact {
 			}
 			else if (line.getC_Charge_ID() > 0) {
 				// TODO
+			}
+		}
+		else if (fact.getAD_Table_ID() == MAllocationHdr.Table_ID && fact.getLine_ID() > 0) {
+			MAllocationLine line = new MAllocationLine(ctx, fact.getLine_ID(), trxName);
+			if (line.getC_Payment_ID() > 0) {
+				MPayment pay = new MPayment(ctx, line.getC_Payment_ID(), trxName);
+				if (pay.getC_Activity_ID() > 0)
+					DB.executeUpdate("Update Fact_Acct set "
+						+ "c_activity_id ="+ pay.getC_Activity_ID() 
+						+ " where Fact_Acct_ID = "+fact.getFact_Acct_ID(), trxName);
 			}
 		};
 	}
