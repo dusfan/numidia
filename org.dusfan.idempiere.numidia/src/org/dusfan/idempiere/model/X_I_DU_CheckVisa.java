f5ff7c7fb628341ffb5e2effe89e85fd1b05bbcf
mde <mde@localhost>
2018-11-18 13:38:40 +0100

    class for table I_DU_CheckVisa

---
+++ org.dusfan.idempiere.numidia/src/org/dusfan/idempiere/model/X_I_DU_CheckVisa.java
@@ -0,0 +1,269 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.dusfan.idempiere.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Model for I_DU_CheckVisa
+ *  @author iDempiere (generated) 
+ *  @version Release 4.1 - $Id$ */
+public class X_I_DU_CheckVisa extends PO implements I_I_DU_CheckVisa, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20181118L;
+
+    /** Standard Constructor */
+    public X_I_DU_CheckVisa (Properties ctx, int I_DU_CheckVisa_ID, String trxName)
+    {
+      super (ctx, I_DU_CheckVisa_ID, trxName);
+      /** if (I_DU_CheckVisa_ID == 0)
+        {
+			setI_DU_CheckVisa_ID (0);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_I_DU_CheckVisa (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 3 - Client - Org 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_I_DU_CheckVisa[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Description.
+		@param Description 
+		Optional short description of the record
+	  */
+	public void setDescription (String Description)
+	{
+		set_Value (COLUMNNAME_Description, Description);
+	}
+
+	/** Get Description.
+		@return Optional short description of the record
+	  */
+	public String getDescription () 
+	{
+		return (String)get_Value(COLUMNNAME_Description);
+	}
+
+	/** Set Taxe Pélerin (Omra).
+		@param I_DU_CheckVisa_ID Taxe Pélerin (Omra)	  */
+	public void setI_DU_CheckVisa_ID (int I_DU_CheckVisa_ID)
+	{
+		if (I_DU_CheckVisa_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_I_DU_CheckVisa_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_I_DU_CheckVisa_ID, Integer.valueOf(I_DU_CheckVisa_ID));
+	}
+
+	/** Get Taxe Pélerin (Omra).
+		@return Taxe Pélerin (Omra)	  */
+	public int getI_DU_CheckVisa_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_I_DU_CheckVisa_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set I_DU_CheckVisa_UU.
+		@param I_DU_CheckVisa_UU I_DU_CheckVisa_UU	  */
+	public void setI_DU_CheckVisa_UU (String I_DU_CheckVisa_UU)
+	{
+		set_Value (COLUMNNAME_I_DU_CheckVisa_UU, I_DU_CheckVisa_UU);
+	}
+
+	/** Get I_DU_CheckVisa_UU.
+		@return I_DU_CheckVisa_UU	  */
+	public String getI_DU_CheckVisa_UU () 
+	{
+		return (String)get_Value(COLUMNNAME_I_DU_CheckVisa_UU);
+	}
+
+	/** Set Import Error Message.
+		@param I_ErrorMsg 
+		Messages generated from import process
+	  */
+	public void setI_ErrorMsg (String I_ErrorMsg)
+	{
+		set_Value (COLUMNNAME_I_ErrorMsg, I_ErrorMsg);
+	}
+
+	/** Get Import Error Message.
+		@return Messages generated from import process
+	  */
+	public String getI_ErrorMsg () 
+	{
+		return (String)get_Value(COLUMNNAME_I_ErrorMsg);
+	}
+
+	/** Set Imported.
+		@param I_IsImported 
+		Has this import been processed
+	  */
+	public void setI_IsImported (boolean I_IsImported)
+	{
+		set_Value (COLUMNNAME_I_IsImported, Boolean.valueOf(I_IsImported));
+	}
+
+	/** Get Imported.
+		@return Has this import been processed
+	  */
+	public boolean isI_IsImported () 
+	{
+		Object oo = get_Value(COLUMNNAME_I_IsImported);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Name.
+		@param Name 
+		Alphanumeric identifier of the entity
+	  */
+	public void setName (String Name)
+	{
+		set_Value (COLUMNNAME_Name, Name);
+	}
+
+	/** Get Name.
+		@return Alphanumeric identifier of the entity
+	  */
+	public String getName () 
+	{
+		return (String)get_Value(COLUMNNAME_Name);
+	}
+
+	/** Set Name 2.
+		@param Name2 
+		Additional Name
+	  */
+	public void setName2 (String Name2)
+	{
+		set_Value (COLUMNNAME_Name2, Name2);
+	}
+
+	/** Get Name 2.
+		@return Additional Name
+	  */
+	public String getName2 () 
+	{
+		return (String)get_Value(COLUMNNAME_Name2);
+	}
+
+	/** Set Processed.
+		@param Processed 
+		The document has been processed
+	  */
+	public void setProcessed (boolean Processed)
+	{
+		set_Value (COLUMNNAME_Processed, Boolean.valueOf(Processed));
+	}
+
+	/** Get Processed.
+		@return The document has been processed
+	  */
+	public boolean isProcessed () 
+	{
+		Object oo = get_Value(COLUMNNAME_Processed);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Process Now.
+		@param Processing Process Now	  */
+	public void setProcessing (boolean Processing)
+	{
+		set_Value (COLUMNNAME_Processing, Boolean.valueOf(Processing));
+	}
+
+	/** Get Process Now.
+		@return Process Now	  */
+	public boolean isProcessing () 
+	{
+		Object oo = get_Value(COLUMNNAME_Processing);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Search Key.
+		@param Value 
+		Search key for the record in the format required - must be unique
+	  */
+	public void setValue (String Value)
+	{
+		set_Value (COLUMNNAME_Value, Value);
+	}
+
+	/** Get Search Key.
+		@return Search key for the record in the format required - must be unique
+	  */
+	public String getValue () 
+	{
+		return (String)get_Value(COLUMNNAME_Value);
+	}
+
+    /** Get Record ID/ColumnName
+        @return ID/ColumnName pair
+      */
+    public KeyNamePair getKeyNamePair() 
+    {
+        return new KeyNamePair(get_ID(), getValue());
+    }
+}
