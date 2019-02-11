f90e645ad48ee13c1da3502462900addf5ece058
mde <mde@localhost>
2019-02-11 15:59:15 +0100

    Ajout de logic pour le calcul de b?nifice

--- org.dusfan.idempiere.numidia/src/org/dusfan/idempiere/callout/CalloutNumidiaInvoiceLine.java
+++ org.dusfan.idempiere.numidia/src/org/dusfan/idempiere/callout/CalloutNumidiaInvoiceLine.java
@@ -53,9 +53,9 @@ public class CalloutNumidiaInvoiceLine implements IColumnCallout{
 							.setParameters(new Object[] { pr.getC_TaxCategory_ID() }).first();
 					BigDecimal divider = tax.getRate();
 					divider = (divider.divide(Env.ONEHUNDRED, 2, BigDecimal.ROUND_HALF_UP)).add(Env.ONE);
-					BigDecimal AgencyProfit = (BigDecimal) mTab.getValue("AgencyProfit");
-					AgencyProfit = AgencyProfit.divide(divider, 2, BigDecimal.ROUND_HALF_UP);
-					mTab.setValue("T_OtherDZD", AgencyProfit);
+					BigDecimal PriceEuro = (BigDecimal) mTab.getValue("AgencyProfit");
+					PriceEuro = PriceEuro.divide(divider, 2, BigDecimal.ROUND_HALF_UP);
+					mTab.setValue("PriceEuro", PriceEuro);
 				}
 			}
 
